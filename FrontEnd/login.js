0a0e6314d048a2d908a0b41aa165a19f8c591692
Eugénie Manantsoa <eharisolonirina@yahoo.com>
2023-04-12 19:08:22 +0200

    Authentification de l’utilisateur

---
+++ FrontEnd/login.js
@@ -0,0 +1,40 @@
+document.getElementById("submit").addEventListener("click", function (e) {
+  e.preventDefault(); // évenement par défaut du boutton (se connecter) non appliquer
+  authentication();
+});
+
+async function authentication() {
+  let username = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+  document.getElementById("errormessage").innerText = "";
+  document.getElementById("errormessage").style.display = "none";
+  let user = {
+    email: username,
+    password: password,
+  };
+
+  let response = await fetch("http://localhost:5678/api/users/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify(user),
+  });
+
+  if (response.ok) {
+    let result = await response.json();
+    localStorage.setItem("SessionToken", result.token);
+    console.log(localStorage.getItem("SessionToken"));
+    document.location.href = "index.html";
+  } else if (response.status == 401) {
+    document.getElementById("errormessage").style.display = "block";
+    document.getElementById("errormessage").innerText = "Not Authorized";
+  } else if (response.status == 404) {
+    document.getElementById("errormessage").style.display = "block";
+    document.getElementById("errormessage").innerText = "User not found";
+  } else {
+    document.getElementById("errormessage").style.display = "block";
+    document.getElementById("errormessage").innerText =
+      "Mauvaise réponse du réseau";
+  }
+}
--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,106 +1,70 @@
 // Récuperation API projets de l’architecte
+
 const reponses = await fetch("http://localhost:5678/api/works");
 console.log(reponses);
 const works = await reponses.json();
-// création des balises
-/*let article = works[0];
-let figureElement = document.createElement("figure");
-let roomElement = document.createElement("img");
-roomElement.src = article.imageUrl;
-roomElement.alt = article.title;
-let figcaptionElement = document.createElement("figcaption");
-figcaptionElement.innerText = article.title;*/
-//rattachement des balises au DOM
-const gallery = document.querySelector("#gallery");
-/*gallery.appendChild(figureElement);
-gallery.appendChild(roomElement);
-gallery.appendChild(figcaptionElement);*/
 
+const gallery = document.querySelector("#gallery");
 for (let work of works) {
   let figureElement = document.createElement("figure");
-  let roomElement = document.createElement("img");
-  roomElement.src = work.imageUrl;
-  roomElement.alt = work.title;
+  let imgElement = document.createElement("img");
+  imgElement.src = work.imageUrl;
+  imgElement.alt = work.title;
   let figcaptionElement = document.createElement("figcaption");
   figcaptionElement.innerText = work.title;
-
-  figureElement.appendChild(roomElement);
+  figureElement.appendChild(imgElement);
   figureElement.appendChild(figcaptionElement);
   gallery.appendChild(figureElement);
 }
+
 // : Réalisation du filtre des travaux
 // Récupération des donnés du categorie
+const reponseCategories = await fetch("http://localhost:5678/api/categories");
+const jsonCategories = await reponseCategories.json();
+const categoryFilter = document.querySelector("#filter");
+for (let jsonCategory of jsonCategories) {
+  let buttonElement = document.createElement("button");
+  buttonElement.innerText = jsonCategory.name;
+  buttonElement.id = jsonCategory.id;
+  buttonElement.classList.add("category");
+  categoryFilter.appendChild(buttonElement);
+}
 
-fetch("http://localhost:5678/api/categories")
-  .then(function (response) {
-    if (response.ok) {
-      response.json().then(function (myJson) {
-        let categories = new Set(myJson);
-        console.log(myJson);
-        let buttonOthers = document.createElement("button");
-        buttonOthers.name = 0;
-        buttonOthers.innerText = "Others";
-        buttonOthers.classList.add("category");
-        buttonOthers.classList.add("category_selected");
-        filter.appendChild(buttonOthers);
-        buttonOthers.addEventListener("click", filterWork);
+//ajout de l'évenement au clic
+const filterChidren = document.querySelector("#filter").children;
+for (let filterChild of filterChidren) {
+  let categoryId = filterChild.id;
+  filterChild.addEventListener("click", () => filterWork(categoryId));
+}
 
-        for (let category of categories) {
-          let button = document.createElement("button");
-          button.name = category.id;
-          button.innerText = category.name;
-          button.classList.add("category");
-          filter.appendChild(button);
-          button.addEventListener("click", filterWork);
-        }
-      });
+async function filterWork(categoryId) {
+  let worksFilters = await fetch("http://localhost:5678/api/works");
+  let workFilter = await worksFilters.json();
+
+  let filteredData =
+    categoryId === "0"
+      ? workFilter
+      : workFilter.filter((work) => work.categoryId == categoryId);
+
+  for (let buttoncategory of filterChidren) {
+    if (buttoncategory.id == categoryId) {
+      buttoncategory.classList.add("category_selected");
     } else {
-      console.log("Mauvaise réponse du réseau");
+      buttoncategory.classList.remove("category_selected");
     }
-  })
-  .catch(function (error) {
-    console.log(
-      "Il y a eu un problème avec l'opération fetch : " + error.message
-    );
-  });
+  }
 
-function filterWork(e) {
-  fetch("http://localhost:5678/api/works")
-    .then(function (response) {
-      if (response.ok) {
-        response.json().then(function (myJson) {
-          let filteredData =
-            e.target.name == 0
-              ? myJson
-              : myJson.filter((model) => model.categoryId == e.target.name);
-          gallery.replaceChildren();
-          for (let buttoncategory of filter.children) {
-            if (buttoncategory.name == e.target.name) {
-              buttoncategory.classList.add("category_selected");
-            } else {
-              buttoncategory.classList.remove("category_selected");
-            }
-          }
+  gallery.replaceChildren();
 
-          for (let work of filteredData) {
-            let figure = document.createElement("figure");
-            let img = document.createElement("img");
-            img.src = work.imageUrl;
-            img.alt = work.title;
-            let figcaption = document.createElement("figcaption");
-            figcaption.innerText = work.title;
-            figure.appendChild(img);
-            figure.appendChild(figcaption);
-            gallery.appendChild(figure);
-          }
-        });
-      } else {
-        console.log("Mauvaise réponse du réseau");
-      }
-    })
-    .catch(function (error) {
-      console.log(
-        "Il y a eu un problème avec l'opération fetch : " + error.message
-      );
-    });
+  for (let work of filteredData) {
+    let figure = document.createElement("figure");
+    let img = document.createElement("img");
+    img.src = work.imageUrl;
+    img.alt = work.title;
+    let figcaption = document.createElement("figcaption");
+    figcaption.innerText = work.title;
+    figure.appendChild(img);
+    figure.appendChild(figcaption);
+    gallery.appendChild(figure);
+  }
 }
