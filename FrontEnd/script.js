04a840f5e7ae1d35bb23764b47e92ce38650d0ce
Eugénie Manantsoa <eharisolonirina@yahoo.com>
2023-04-16 22:29:13 +0200

    ajout du modal 1 et 2

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,5 +1,6 @@
 // Redirection de l'user vers les pages adapter
 //Changement de text login logout
+// Gerer l'apparition de la bande noire
 const linkLogin = document.getElementById("link-login");
 const strip = document.getElementById("strip");
 const stripImg = document.getElementById("strip-img");
@@ -103,3 +104,159 @@ async function filterWork(categoryId) {
     gallery.appendChild(figure);
   }
 }
+
+// boite modale
+const pagemodal = document.getElementById("pagemodal");
+const CloseIcon = document.getElementById("close");
+
+//comportement quand on click sur le lien modifier
+striProject.onclick = async () => {
+  pagemodal.style.display = "block";
+  let pagemodalreponses = await fetch("http://localhost:5678/api/works");
+  let pagemodalworks = await pagemodalreponses.json();
+  // le contenu du modale
+  let pagemodalgallery = document.querySelector("#modal-content-gallery");
+  pagemodalgallery.replaceChildren();
+  for (let work of pagemodalworks) {
+    let figureElement = document.createElement("figure");
+
+    let imgElement = document.createElement("img");
+    imgElement.src = work.imageUrl;
+    imgElement.alt = work.title;
+
+    let figcaptionElement = document.createElement("figcaption");
+    figcaptionElement.innerText = "éditer";
+
+    let iconsElement = document.createElement("div");
+    iconsElement.classList.add("modal-content-gallery-img-icons");
+
+    let trashElement = document.createElement("i");
+    trashElement.classList.add("fa");
+    trashElement.classList.add("fa-trash-can");
+    trashElement.classList.add("modalimageicon");
+
+    trashElement.addEventListener("click", () => {
+      deletework(work.id);
+    });
+
+    let expandElement = document.createElement("i");
+    expandElement.classList.add("fa");
+    expandElement.classList.add("fa-arrows-up-down-left-right");
+    expandElement.classList.add("modalimageicon");
+    expandElement.classList.add("displaynone");
+
+    iconsElement.appendChild(expandElement);
+    iconsElement.appendChild(trashElement);
+    figureElement.appendChild(iconsElement);
+    figureElement.appendChild(imgElement);
+
+    figureElement.appendChild(figcaptionElement);
+    pagemodalgallery.appendChild(figureElement);
+  }
+};
+
+// fermeture du modale avec le bouton X
+CloseIcon.addEventListener("click", function () {
+  pagemodal.style.display = "none";
+});
+// un click en dehors de la fenêtre modale le ferme
+document.addEventListener("click", function (event) {
+  if (event.target == pagemodal) {
+    pagemodal.style.display = "none";
+  }
+});
+
+async function deletework(workid) {
+  await fetch("http://localhost:5678/api/works/" + workid, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${localStorage.getItem("SessionToken")}`,
+    },
+  });
+
+  let pagemodalreponses = await fetch("http://localhost:5678/api/works");
+  let pagemodalworks = await pagemodalreponses.json();
+
+  let pagemodalgallery = document.querySelector("#modal-content-gallery");
+  pagemodalgallery.replaceChildren();
+  for (let work of pagemodalworks) {
+    let figureElement = document.createElement("figure");
+
+    let imgElement = document.createElement("img");
+    imgElement.src = work.imageUrl;
+    imgElement.alt = work.title;
+
+    let figcaptionElement = document.createElement("figcaption");
+    figcaptionElement.innerText = "éditer";
+
+    let iconsElement = document.createElement("div");
+    iconsElement.classList.add("modal-content-gallery-img-icons");
+
+    let trashElement = document.createElement("i");
+    trashElement.classList.add("fa");
+    trashElement.classList.add("fa-trash-can");
+    trashElement.classList.add("modalimageicon");
+
+    trashElement.addEventListener("click", () => {
+      deletework(work.id);
+    });
+
+    let expandElement = document.createElement("i");
+    expandElement.classList.add("fa");
+    expandElement.classList.add("fa-arrows-up-down-left-right");
+    expandElement.classList.add("modalimageicon");
+    expandElement.classList.add("displaynone");
+
+    iconsElement.appendChild(expandElement);
+    iconsElement.appendChild(trashElement);
+    figureElement.appendChild(iconsElement);
+    figureElement.appendChild(imgElement);
+
+    figureElement.appendChild(figcaptionElement);
+    pagemodalgallery.appendChild(figureElement);
+  }
+}
+
+const deleteall = document.getElementById("deleteall");
+deleteall.addEventListener("click", () => {
+  deleteallworks();
+});
+
+async function deleteallworks() {
+  let workreponses = await fetch("http://localhost:5678/api/works");
+
+  let works = await workreponses.json();
+  for (let work of works) {
+    let deleteresponse = await fetch(
+      "http://localhost:5678/api/works/" + work.id,
+      {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("SessionToken")}`,
+        },
+      }
+    );
+  }
+  let pagemodalgallery = document.querySelector("#modal-content-gallery");
+  pagemodalgallery.replaceChildren();
+}
+
+const addphoto = document.getElementById("addphoto");
+const leftarrow = document.getElementById("left");
+addphoto.addEventListener("click", () => {
+  let modalcontenttwo = document.getElementById("modal-content-two");
+  let modalcontentone = document.getElementById("modal-content-one");
+  modalcontenttwo.classList.remove("displaynone");
+  leftarrow.classList.remove("visibilityhidden");
+  modalcontentone.classList.add("displaynone");
+});
+
+left.addEventListener("click", () => {
+  let modalcontenttwo = document.getElementById("modal-content-two");
+  let modalcontentone = document.getElementById("modal-content-one");
+  modalcontenttwo.classList.add("displaynone");
+  leftarrow.classList.add("visibilityhidden");
+  modalcontentone.classList.remove("displaynone");
+});
